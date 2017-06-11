c2df9e6b8e9c3b0d3c7d7b4c56b5aed4490ea958
yoda <baloneymath@gmail.com>
2017-06-11 15:21:25 +0800

    fix run

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 ./steiner testbench/case3 -plot case3.plt -out case3.out
 ./steiner testbench/case4 -plot case4.plt -out case4.out
 ./steiner testbench/case5 -plot case5.plt -out case5.out
-./steiner testbench/case6 -plot case6.plt -out csae6.out
+./steiner testbench/case6 -plot case6.plt -out case6.out
 ./steiner testbench/case7 -plot case7.plt -out case7.out
 ./steiner testbench/case8 -plot case8.plt -out case8.out
